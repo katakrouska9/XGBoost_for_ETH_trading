1a40b7cfd467c3e6b5cc4c68caf182afff114176
katakrouska9 <kata.krouska9@gmail.com>
2026-02-18 13:43:11 +0000

    Data load

--- ML_strategy.py
+++ ML_strategy.py
@@ -3,6 +3,28 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import numpy as np
 import yfinance as yf
+import pandas as pd
+
+
+# Download price
+
+ticker_1 = "BTC-USD"
+ticker_2 = "ETH-USD"
+
+pd.set_option("display.max_columns", None)
+
+data = yf.download("ETH-USD", period="730d", interval="1h")
+#plt.plot(data['Close'])
+#plt.show()
+
+#Checking summary statistics
+
+data.head()
+data.isna().sum() #no n/a
+data.describe()
+data.count() 
+
+
 
 
 
